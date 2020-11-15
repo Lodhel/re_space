2d6c4aa8a0873cc0794d65cecf13bbdbedd5a08e
Lodhel <lodhelnew@gmail.com>
2020-11-15 19:18:22 +0300

    ssl protocol

--- core/main.py
+++ core/main.py
@@ -1,3 +1,6 @@
+import os
+import ssl
+
 from aiohttp import web
 
 from views import websocket_registration, websocket_login, websocket_item, websocket_echo
@@ -16,4 +19,11 @@ app.add_routes([
     web.get('/api/echo/', websocket_echo)
 ])
 
-web.run_app(app)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+crt = "{}/{}".format(BASE_DIR, "domain_srv.crt")
+key = "{}/{}".format(BASE_DIR, "domain_srv.key")
+ssl_context.load_cert_chain(crt, key)
+
+web.run_app(app, ssl_context=ssl_context)
